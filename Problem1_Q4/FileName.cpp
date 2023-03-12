1df11acff4eb94e7c4c9e24a8145d201b0fe5c80
ahmedgaboelnaga <ahmedgaboelnaga@gmail.com>
2023-03-12 21:34:35 +0200

    Add project files.

---
+++ Problem1_Q4/FileName.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	double n1, n2, sum, different, product, quotent, reaminder;
+	cout << "Enter the first number: ";
+	cin >> n1;
+	
+	cout << "Enter the second number: ";
+	cin >> n2;
+
+	cout << endl;
+
+	cout << "Sum: " << n1 + n2 << endl;
+	cout << "Different: " << n1 - n2 << endl;
+	cout << "Product: " << n1 * n2 << endl;
+	cout << "Quotent: " << n1 / n2 << endl;
+	cout << "Modulus " << (int)n1 % (int)n2 << endl;
+
+	return 0;
+}
